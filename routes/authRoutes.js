0a79937f2a202574e44e3153ffc32b99a8d47730
Nate K <thekingfamilybiz@gmail.com>
2023-03-05 07:17:40 -0700

    debugging

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,70 +1,85 @@
 const express = require('express');
-const passport = require('passport');
-const { check, validationResult } = require('express-validator');
-const authController = require('../controllers/authController');
+const bcrypt = require('bcryptjs');
+const db = require('./db');
 
 const router = express.Router();
 
 // GET request for login form
-router.get('/login', authController.loginForm);
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 
 // POST request for login form
-router.post(
-  '/login',
-  [
-    // Validate email and password
-    check('email').isEmail().withMessage('Invalid email address'),
-    check('password')
-      .notEmpty()
-      .withMessage('Password must not be empty')
-      .isLength({ min: 8 })
-      .withMessage('Password must be at least 8 characters long'),
-  ],
-  authController.login
-);
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user by email
+    const user = await db.User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    // Check if password matches
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    // Save user id to session
+    req.session.userId = user.id;
+
+    res.redirect('/dashboard');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 // GET request for signup form
-router.get('/signup', authController.signupForm);
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
 
 // POST request for signup form
-router.post(
-  '/signup',
-  [
-    // Validate name, email, and password
-    check('name').notEmpty().withMessage('Name must not be empty'),
-    check('email').isEmail().withMessage('Invalid email address'),
-    check('password')
-      .notEmpty()
-      .withMessage('Password must not be empty')
-      .isLength({ min: 8 })
-      .withMessage('Password must be at least 8 characters long'),
-    check('confirmPassword')
-      .notEmpty()
-      .withMessage('Confirm password must not be empty')
-      .custom((value, { req }) => {
-        if (value !== req.body.password) {
-          throw new Error('Passwords do not match');
-        }
-        return true;
-      }),
-  ],
-  authController.signup
-);
+router.post('/signup', async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
 
-// GET request for logout
-router.get('/logout', authController.logout);
+  // Validate name, email, password, and confirmPassword
+  if (!name || !email || !password || !confirmPassword) {
+    return res.status(400).json({ msg: 'Please enter all fields' });
+  }
 
-// GET request for Google OAuth authentication
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+  if (password !== confirmPassword) {
+    return res.status(400).json({ msg: 'Passwords do not match' });
+  }
 
-// GET request for Google OAuth callback
-router.get(
-  '/google/callback',
-  passport.authenticate('google', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/login',
-    failureFlash: true,
-  })
-);
+  if (password.length < 8) {
+    return res.status(400).json({ msg: 'Password must be at least 8 characters long' });
+  }
+
+  try {
+    // Check if user already exists
+    let user = await db.User.findOne({ where: { email } });
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    user = await db.User.create({ name, email, password: hashedPassword });
+
+    // Save user id to session
+    req.session.userId = user.id;
+
+    res.redirect('/dashboard');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
