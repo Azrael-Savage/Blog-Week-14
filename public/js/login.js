8e8736bcf3bb6a1a01112e00cd1c55b599f34996
Athena King <113001155+Azrael-Savage@users.noreply.github.com>
2023-03-12 11:58:16 -0600

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,27 @@
-const submitBtn = document.getElementById("login-send");
+async function loginFormHandler(event) {
+    event.preventDefault();
 
-submitBtn.addEventListener("click", async function loginSubmit(e) {
-  e.preventDefault();
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
 
-  const username = document.getElementById("username-login").value.trim();
-  const password = document.getElementById("password-login").value.trim();
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
-  if (username && password) {
-    const response = await fetch("/api/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password }),
-    });
-
-    if (response.ok) {
-      document.location.replace(`/`);
-    } else {
-      alert("Failed to log in");
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
     }
-  }
-});
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
